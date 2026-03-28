8073bc4c79cf120b338e95fc85c1e5bfd1d2d0a8
mravec <48887485+mravec@users.noreply.github.com>
2026-03-28 15:30:32 +0100

    melodie

--- main.ts
+++ main.ts
@@ -5,6 +5,23 @@ radio.onReceivedValue(function (name, value) {
     if (name == "y") {
         yHodnota = value
     }
+    if (name == "t") {
+        if (value == 1) {
+            cuteBot.singleheadlights(cuteBot.RGBLights.RGB_R, 255, 255, 0)
+        } else if (value == 2) {
+            cuteBot.singleheadlights(cuteBot.RGBLights.RGB_L, 0, 0, 255)
+        } else if (value == 3) {
+            music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.InBackground)
+        } else if (value == 4) {
+            music.play(music.stringPlayable("B B B G G G A A ", 120), music.PlaybackMode.InBackground)
+        } else if (value == 5) {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+        } else if (value == 6) {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+        } else {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Chase), music.PlaybackMode.InBackground)
+        }
+    }
 })
 let yHodnota = 0
 let xHodnota = 0
